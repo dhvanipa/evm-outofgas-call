b5f63534df12dae21cb36e7e7f5ad45ae7db7298
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-08-09 18:23:24 +0100

    update comment

--- src/TestContracts.sol
+++ src/TestContracts.sol
@@ -25,12 +25,13 @@ contract WorldContract {
 
     (bool success, ) = address(chipContractAddress).call(abi.encodeWithSignature("onMine()"));
 
-    // A dummy operation post calling the chip contract to consume gas
-    // to trigger out of gas exception, as a simple return does not
-    // if (!success) {
-    //   for (uint256 i = 0; i < 100; i++) {
-    //     continue;
-    //   }
-    // }
+    if (!success) {
+      // A dummy operation post calling the chip contract to consume gas
+      // to trigger out of gas exception, as a simple return does not
+      // Note: smaller operations < 10 do not consume enough gas to trigger out of gas exception
+      // for (uint256 i = 0; i < 10; i++) {
+      //   continue;
+      // }
+    }
   }
 }
